3e836ff0530b65474c500f858878f8054b086148
Sara Elframawy <sara.elfaramawy.93@gmail.com>
2023-04-20 09:53:16 +0200

    ny version

--- src/main/java/org/example/Operations.java
+++ src/main/java/org/example/Operations.java
@@ -1,20 +1,20 @@
 package org.example;
 
 public class Operations {
-//@Step("add number1 and number2")
+
     public int add (int number1, int number2){
         return  number1+number2;
     }
-    //@Step("subtract {number1} and {number2}")
+
     public int sub (int number1,int number2){
         return number1-number2;
     }
-    //@Step("multi {number1} and {number2}")
+
     public int multi(int number1, int number2){
         return number1*number2;
     }
 
-    //@Step("divide {number1} and {number2}")
+
     public int divide(int number1, int number2){
         return number1/number2;
 
